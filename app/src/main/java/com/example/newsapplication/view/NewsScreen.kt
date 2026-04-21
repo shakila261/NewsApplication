2be6b7d440d4c92527cc6e6cf7fa6b9828594856
shakila <shakilanaik2002@gmail.com>
2026-04-21 10:03:33 +0530

    third commit

--- app/src/main/java/com/example/newsapplication/view/NewsScreen.kt
+++ app/src/main/java/com/example/newsapplication/view/NewsScreen.kt
@@ -84,7 +84,7 @@ fun NewsScreen(
                     "TODAYS NEWS",
                     fontWeight = FontWeight.Bold,
                     fontSize = 32.sp,
-                    color = Color.Cyan,
+                    color = Color.Blue,
                     textAlign = TextAlign.Center,
                     modifier = Modifier.fillMaxWidth()
 
